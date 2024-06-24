188bee9cbac4e3a7f7d0b6f2f1a253f389f0705d
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-06-24 13:11:22 +0900

    Initial Commit

---
+++ BlockChainAssetTrader/BlockChainAssetTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockChainAssetTraderApp.swift
+//  BlockChainAssetTrader
+//
+//  Created by JONGHUN PARK on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BlockChainAssetTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
